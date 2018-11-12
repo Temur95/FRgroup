c6cdf875582f2f3ac4f2a05f1a0941f1797d8048
Temur95 <timurm756@gmail.com>
2018-11-12 12:32:45 +0500

    Merge remote-tracking branch 'Spiders/master'
    # Conflicts:
    #	.idea/misc.xml
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/example/ulugbek/frgroup/Login.java

--- app/src/main/java/com/example/ulugbek/frgroup/Login.java
+++ app/src/main/java/com/example/ulugbek/frgroup/Login.java
@@ -2,7 +2,6 @@ package com.example.ulugbek.frgroup;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.AsyncTask;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,10 +11,8 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.net.Socket;
-
 public class Login extends AppCompatActivity {
-    EditText t1, t2;
+    EditText username, password;
     Button log;
     String pass, login;
     TextView reg;
@@ -26,17 +23,16 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-//        login = "fr";
-//        pass = "fr";
-//
-//        t1 = (EditText) findViewById(R.id.logText);
-//        t2 = (EditText) findViewById(R.id.passText);
-//        log = (Button) findViewById(R.id.btn);
-//        reg = (TextView) findViewById(R.id.regTxt);
+        login = "fr";
+        pass = "fr";
 
-<<<<<<< HEAD
-        t1 = (EditText) findViewById(R.id.logText);
-        t2 = (EditText) findViewById(R.id.passText);
+        username = (EditText) findViewById(R.id.logText);
+        password = (EditText) findViewById(R.id.passText);
+        log = (Button) findViewById(R.id.btn);
+        reg = (TextView) findViewById(R.id.regTxt);
+
+        username = (EditText) findViewById(R.id.logText);
+        password = (EditText) findViewById(R.id.passText);
         log = (Button) findViewById(R.id.btn);
         reg = (TextView) findViewById(R.id.regTxt);
 
@@ -50,11 +46,11 @@ public class Login extends AppCompatActivity {
         });
         log.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
-                if(t1.getText().toString().equals(login) && t2.getText().toString().equals(pass)) {
+                if(username.getText().toString().equals(login) && password.getText().toString().equals(pass)) {
                     SharedPreferences sPref = getSharedPreferences("login",MODE_PRIVATE);
                     SharedPreferences.Editor ed = sPref.edit();
-                    ed.putString("username", t1.getText().toString());
-                    ed.putString("password", t2.getText().toString());
+                    ed.putString("username", username.getText().toString());
+                    ed.putString("password", password.getText().toString());
                     ed.commit();
                     Intent move = new Intent("com.ulugbek.frgroup.MAINACTIVITY");
                     startActivity(move);
@@ -69,42 +65,40 @@ public class Login extends AppCompatActivity {
                 }
             }
         });
-=======
-//        SharedPreferences sPref = getPreferences(MODE_PRIVATE);
-//        String username = sPref.getString("username","");
-//        if(username.equals("fr")){
-//            Intent move = new Intent("com.ulugbek.frgroup.MAINACTIVITY");
-//            startActivity(move);
-//            finish();
-//        }
-//        reg.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent move = new Intent("com.ulugbek.frgroup.REGISTER");
-//                startActivity(move);
-//            }
-//        });
-//        log.setOnClickListener(new View.OnClickListener(){
-//            public void onClick(View v){
-//                if(t1.getText().toString().equals(login) && t2.getText().toString().equals(pass)) {
-//                    SharedPreferences sPref = getPreferences(MODE_PRIVATE);
-//                    SharedPreferences.Editor ed = sPref.edit();
-//                    ed.putString("username", t1.getText().toString());
-//                    ed.commit();
-//                    Intent move = new Intent("com.ulugbek.frgroup.MAINACTIVITY");
-//                    startActivity(move);
-//                    finish();
-//
-//                }
-//                else
-//                {
-//                    Toast toast = Toast.makeText(getApplicationContext(),
-//                            "Error", Toast.LENGTH_SHORT);
-//                    toast.show();
-//                }
-//            }
-//        });
->>>>>>> Spiders/master
+        SharedPreferences sPref = getPreferences(MODE_PRIVATE);
+        String username = sPref.getString("username","");
+        if(username.equals("fr")){
+            Intent move = new Intent("com.ulugbek.frgroup.MAINACTIVITY");
+            startActivity(move);
+            finish();
+        }
+        reg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                   Intent move = new Intent("com.ulugbek.frgroup.REGISTER");
+                startActivity(move);
+            }
+        });
+        log.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                if(Login.this.username.getText().toString().equals(login) && password.getText().toString().equals(pass)) {
+                    SharedPreferences sPref = getPreferences(MODE_PRIVATE);
+                    SharedPreferences.Editor ed = sPref.edit();
+                    ed.putString("username", Login.this.username.getText().toString());
+                    ed.commit();
+                    Intent move = new Intent("com.ulugbek.frgroup.MAINACTIVITY");
+                    startActivity(move);
+                    finish();
+
+                }
+                else
+                {
+                    Toast toast = Toast.makeText(getApplicationContext(),
+                            "Error", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+            }
+        });
 
 
     }
